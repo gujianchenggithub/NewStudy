feddae6ce0c0ff6f2d6f81b0aadd9975edd23fd3
gujiancheng <gujiancheng1989@163.com>
2020-02-08 19:37:49 +0800

    runtest001

--- Test_Project/runtest.py
+++ Test_Project/runtest.py
@@ -6,7 +6,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from operateDB import operateDb
-
+#1111
 
 def send_mail(latest_report):
     f=open(latest_report,'rb')
